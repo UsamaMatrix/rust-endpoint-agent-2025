04c7cdee13c1ce375876928a57b993e8e095a9ac
UsamaMatrix <192347513+UsamaMatrix@users.noreply.github.com>
2025-09-13 05:09:30 -0400

    chore: touch rust to refresh linguist stats

--- agent/src/lib.rs
+++ agent/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod config; pub mod logging; pub mod collectors; pub mod transport; pub mod service;
 #[cfg(test)] mod tests{ use super::config::merge_config; use proptest::prelude::*; proptest!{
 #[test] fn merge_config_prefers_b_over_a(s in ".*"){ let a=super::config::AgentConfig::default(); let mut b=a.clone(); b.common.instance_id=s.clone(); let m=merge_config(&a,&b); prop_assert_eq!(m.common.instance_id,s); } } }
+// touch
